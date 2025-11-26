265d93e9af267036ce24d5cd8bc108ccd3c3649d
Snehasish Dey <snehasishdey333@gmail.com>
2025-11-27 01:36:02 +0530

    initial commit

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 class CartPage:
@@ -9,4 +12,7 @@ class CartPage:
     ITEM_NAME=(By.CLASS_NAME,"inventory_item_name")
 
     def proceed_to_checkout_page(self):
-        self.driver.find_element(*self.CHECKOUT_BTN).click()
+        # self.driver.find_element(*self.CHECKOUT_BTN).click()
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.CHECKOUT_BTN)
+        ).click()
